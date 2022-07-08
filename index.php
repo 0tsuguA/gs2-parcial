76f3cbf453920a27d87d4194a7bfa0d96685fa9c
0tsuguA <augusstotl@gmail.com>
2022-07-08 19:28:21 -0300

    apellido

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
-echo 'El nombre ingresado es: '. $_POST['nombre'];
+echo 'El nombre ingresado es: ' . $_POST['nombre'] . '<br>';
+
+
+echo 'El apellido ingresado es: ' . $_POST['apellido'];
